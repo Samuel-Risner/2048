6f5b859c311c2f97e868a9571ae66e0f26d08c26
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-11-14 17:39:27 +0100

    Improved styling

--- src/tile.ts
+++ src/tile.ts
@@ -1,38 +1,108 @@
 import settings from "./settings";
 
-function setStyleInitial(el: HTMLElement) {
+function setStyleInitialPositionElement(el: HTMLElement) {
     el.style.position = "absolute";
     el.style.width = `${settings.style.widthPerUnitPx}px`;
     el.style.height = `${settings.style.widthPerUnitPx}px`;
+    el.style.display = "flex";
 }
 
 function setStyle(el: HTMLElement, bgColor: string, x: number, y: number) {
-    el.style.backgroundColor = bgColor;
     el.style.top = `${y * settings.style.widthPerUnitPx}px`;
     el.style.left = `${x * settings.style.widthPerUnitPx}px`;
 }
 
+function setStyleInitialColorElement(el: HTMLDivElement) {
+    el.style.backgroundColor = "white";
+    el.style.width = "90%";
+    el.style.height = "90%";
+    el.style.display = "flex";
+    el.style.margin = "auto";
+    el.style.borderRadius = "10%";
+}
+
+function setStyleInitialTextElement(el: HTMLDivElement) {
+    el.style.display = "flex";
+    el.style.margin = "auto";
+    el.style.fontSize = `${settings.style.widthPerUnitPx / 3}px`;
+}
+
 export default class Tile {
 
-    private element: HTMLDivElement;
+    private positionElement: HTMLDivElement;
+    private colorElement: HTMLDivElement;
+    private textElement: HTMLDivElement;
+
     private value!: number;
     private hasMerged: boolean;
 
     constructor(private x: number, private y: number, root: HTMLDivElement) {
-        this.element = document.createElement("div");
+        this.positionElement = document.createElement("div");
+        this.colorElement = document.createElement("div");
+        this.textElement = document.createElement("div");
 
-        setStyleInitial(this.element);
-        setStyle(this.element, "red", x, y);
+        setStyleInitialPositionElement(this.positionElement);
+        setStyle(this.positionElement, "red", x, y);
+        setStyleInitialColorElement(this.colorElement);
+        setStyleInitialTextElement(this.textElement);
         this.setValue(2);
 
-        root.appendChild(this.element);
+        root.appendChild(this.positionElement);
+        this.positionElement.appendChild(this.colorElement);
+        this.colorElement.appendChild(this.textElement);
 
         this.hasMerged = false;
     }
 
     setValue(v: number) {
         this.value = v;
-        this.element.textContent = String(v);
+        this.textElement.textContent = String(v);
+
+        switch(v) {
+            case 2:
+                this.colorElement.style.backgroundColor = "#FCD0A1";
+                break;
+
+            case 4:
+                this.colorElement.style.backgroundColor = "#B1B695";
+                break;
+
+            case 8:
+                this.colorElement.style.backgroundColor = "#A690A4";
+                break;
+
+            case 16:
+                this.colorElement.style.backgroundColor = "#5E4B56";
+                break;
+
+            case 32:
+                this.colorElement.style.backgroundColor = "#AFD2E9";
+                break;
+
+            case 64:
+                this.colorElement.style.backgroundColor = "#F42272";
+                break;
+
+            case 128:
+                this.colorElement.style.backgroundColor = "#F397D6";
+                break;
+
+            case 256:
+                this.colorElement.style.backgroundColor = "#D7B8F3";
+                break;
+
+            case 512:
+                this.colorElement.style.backgroundColor = "#B8B8F3";
+                break;
+
+            case 2048:
+                this.colorElement.style.backgroundColor = "#deb841";
+                break;
+
+            default:
+                this.colorElement.style.backgroundColor = "white";
+                break; 
+        }
     }
 
     getValue(): number {
@@ -56,11 +126,11 @@ export default class Tile {
     }
 
     remove() {
-        this.element.remove();
+        this.positionElement.remove();
     }
 
     setAnimation(destinationX: number, destinationY: number, newValue: number, mergeWith: Tile | null) {
-        const animation = this.element.animate([
+        const animation = this.positionElement.animate([
             {
                 left: `${this.x * settings.style.widthPerUnitPx}px`,
                 top: `${this.y * settings.style.widthPerUnitPx}px`
@@ -82,7 +152,7 @@ export default class Tile {
             mergeWith?.remove();
 
             this.setValue(newValue);
-            setStyle(this.element, "aquamarine", this.x, this.y);
+            setStyle(this.positionElement, "aquamarine", this.x, this.y);
         };
     }
     
